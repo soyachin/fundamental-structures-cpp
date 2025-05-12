367d2285f5670d852c573a570c9954b49b019cb0
Kris Salazar <miranda.salazar@utec.edu.pe>
2025-05-12 18:51:44 -0500

    feat: added abstract iterator class

--- include/Array.h
+++ include/Array.h
@@ -4,12 +4,59 @@
 
 #ifndef ARRAY_H
 #define ARRAY_H
+#include <cstddef>
 
+template <typename T>
+class ArrayIterator {
+    private:
+      T** ptr;
+    public:
+        explicit ArrayIterator(T** point = nullptr) : ptr(point) {}
 
+        T& operator*() const { return **ptr; }
+        T* operator->() const { return *ptr; }
 
-class Array {
+        ArrayIterator& operator++() { ++ptr; return *this; }    // Prefix ++
+        ArrayIterator& operator--() { --ptr; return *this; }    // Prefix --
+
+        bool operator==(const ArrayIterator& other) const { return ptr == other.ptr; }
+        bool operator!=(const ArrayIterator& other) const { return ptr != other.ptr; }
+        bool operator<(const ArrayIterator& other) const { return ptr < other.ptr; }
+        bool operator>(const ArrayIterator& other) const { return ptr > other.ptr; }
+        bool operator<=(const ArrayIterator& other) const { return ptr <= other.ptr; }
+        bool operator>=(const ArrayIterator& other) const { return ptr >= other.ptr; }
+
+        ArrayIterator operator+(size_t n) const { return ArrayIterator(ptr + n); }
+        ArrayIterator operator-(size_t n) const { return ArrayIterator(ptr - n); }
+        size_t operator-(const ArrayIterator& other) const { return ptr - other.ptr; }  // Distance
+        ArrayIterator& operator+=(size_t n) { ptr += n; return *this; }
+        ArrayIterator& operator-=(size_t n) { ptr -= n; return *this; }
+        T& operator[](size_t n) const { return **(ptr + n); }
 
 };
+
+template <typename T>
+class Array {
+      T** data;
+      size_t size;
+      size_t capacity;
+      typedef ArrayIterator<T> iterator;
+    void resize(size_t new_capacity);
+    public:
+        iterator begin() { return iterator(data); }
+        iterator end() { return iterator(data + size); }
+        Array(size_t initial_capacity = 10);
+        ~Array();
+        void push_back(const T& value);
+        void pop_back();
+        T& operator[](size_t index);
+        const T& operator[](size_t index) const;
+        size_t get_size() const;
+        size_t get_capacity() const;
+        void clear();
+};
+
+
 
 
 
--- src/Array.cpp
+++ src/Array.cpp
@@ -2,4 +2,74 @@
 // Created by hojas on 12/05/25.
 //
 
-#include "Array.h"
+#include <stdexcept>
+
+#include "../include/Array.h"
+
+template <typename T>
+Array<T>::Array(size_t initial_capacity) : size(0), capacity(initial_capacity) {
+    data = new T[capacity]();
+}
+
+template <typename T>
+Array<T>::~Array() {
+    clear();
+    delete[] data;
+}
+
+template <typename T>
+void Array<T>::push_back(const T& value) {
+    if (size == capacity)
+        resize(capacity * 2);
+
+    data[size] = new T(value);
+    size++;
+
+}
+
+template <typename T>
+void Array<T>::pop_back() {
+    if (size == 0)
+        throw std::out_of_range("Array is empty");
+
+    delete data[size - 1];
+    data[size - 1] = nullptr; // Evitar dangling pointer
+    --size;
+}
+
+template <typename T>
+T& Array<T>::operator[](size_t index) {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+    return *data[index];
+}
+
+template <typename T>
+void Array<T>::clear() {
+     while(size > 0)
+        pop_back();
+}
+
+template <typename T>
+size_t Array<T>::get_size() const {
+    return size;
+}
+
+template <typename T>
+size_t Array<T>::get_capacity() const {
+    return capacity;
+}
+
+template <typename T>
+void Array<T>::resize(size_t new_capacity){
+  T** new_data = new T*[new_capacity];
+  for (size_t i = 0; i < size; ++i) {
+        new_data[i] = data[i];
+  }
+
+  delete[] data;
+  data = new_data;
+  capacity = new_capacity;
+}
+
